df86c4752c872fb6b03ead890a98e18dabb84a6d
kwall <kurt.wall@excella.com>
2014-11-10 11:55:40 -0500

    Add test for Date filter of Newsroom and scroll function to scroll the page

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,11 +3,12 @@
 # from selenose.cases import SeleniumTestCase
 # from nose.plugins.attrib import attr
 # from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as ec
 
-#finds dropdown button then clicks it
 def click_filter_posts_dropdown(test):
+    """Finds dropdown button then clicks it"""
     test.filter_dropdown_button = test.driver.find_element_by_xpath('//button[contains(text(), "Filter posts")]')
     if test.filter_dropdown_button.get_attribute('aria-pressed') == 'false':
         test.filter_dropdown_button.click()
@@ -19,3 +20,7 @@ def coerce_category_for_dom(category):
         category = category.replace('+', ' ')
 
     return category
+
+def scroll(driver):
+    body = driver.find_element_by_xpath('//body')
+    body.send_keys(Keys.SPACE)
